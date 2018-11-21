20526242b19a535737bbafbf5ce51dc5bd1052f7
Robert 808 <hire@robertalbus.com>
2018-11-20 17:19:36 -0700

    Create Map2D class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
-using namespace std;
+#include "Cell.h"
 
 int main()
 {
+    Cell c1;
+    Cell c2;
+    Cell c3;
+    Cell c4;
+
+
+
+    c1 + c2 + c3 + c4;
+
+    // does not work
+    std::cout << c1.countAliveNeighbours();
 
     return 0;
 }
+
